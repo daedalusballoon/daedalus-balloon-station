84aa25dbb4f4d1975e6fa8bcd268b600895e17d5
Kerlin Michel <michelkerlin@gmail.com>
2017-02-07 16:30:02 -0500

    began working user input

--- daedalus-balloon-application/src/main/java/daedalusballoon/application/MainWindow.java
+++ daedalus-balloon-application/src/main/java/daedalusballoon/application/MainWindow.java
@@ -4,7 +4,8 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,8 +14,15 @@ public class MainWindow extends Application {
     public void start(Stage primaryStage) {
         Label title = new Label("Daedalus Balloon");
 
+        Label label1 = new Label("NOAA Station Id:");
+        TextField textField = new TextField ();
+        HBox hb = new HBox();
+        hb.getChildren().addAll(label1, textField);
+        hb.setSpacing(10);
+
         StackPane root = new StackPane();
         root.getChildren().add(title);
+        root.getChildren().add(hb);
 
         Scene scene = new Scene(root, 640, 480);
 
